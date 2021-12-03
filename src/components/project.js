fd16c305db6ebdcc774c6aa97628919e70b68614
wkropat <wkropat@gmail.com>
2021-12-03 07:59:56 -0800

    heroku

--- src/components/project.js
+++ src/components/project.js
@@ -3,15 +3,14 @@
 import React from 'react';
 
 
+
 function Project(props) {
-
-
 
     return (
         <div className="card">
-            <a href="" ><h2 className="cardTitle">Title</h2></a>
+            <a href="" ><h3 className="cardTitle">{props.title}</h3></a>
             <img src="" className="cardImage"/>
-            <p className="cardDesc">Description</p>
+            <p className="cardDesc">{props.description}</p>
         </div>
     );
   }
