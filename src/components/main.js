68a365c5edf67122a28225a531bbe37c14a90a02
wkropat <wkropat@gmail.com>
2021-12-05 09:55:08 -0800

    made seperate key

--- src/components/main.js
+++ src/components/main.js
@@ -4,8 +4,8 @@ var moment = require('moment'); // require
 
 // Build request URL
 const baseURL = `https://api.astronomyapi.com/api/v2/bodies/positions/moon`
-const appId = "b7652871-3d9b-41c5-94b3-34898afc0346";//process.env.REACT_APP_ASTRO_APP_ID;
-const appSecret = "b7b4ce73652c6f54f008d1ab20713ce886a0f39ce6bf8c0185f522e607b7f775f83e61a0a4e6a1716f3ff2d7d2ea8bb72e166d20d282a8c083428fd194ed2ff427f59def7b666555fd4e7d170defdfadf38926e0b8deb7b95cb7869c9aae789e4f567bb187a6dbc5efcf7c4de001e0b2";//process.env.REACT_APP_ASTRO_APP_SECRET;
+const appId = "66850e56-0f2b-4e50-8a0d-439fb6b3db30";//process.env.REACT_APP_ASTRO_APP_ID;
+const appSecret = "b7b4ce73652c6f54f008d1ab20713ce886a0f39ce6bf8c0185f522e607b7f775f83e61a0a4e6a1716f3ff2d7d2ea8bb72e166d20d282a8c083428fd194ed2ff448cda3fb039d5f92635320602a3a4f85c1f02c2dfe65b4bfcc4e996ae730dc1901b557ed08c32947985b8dc6c2848e00";//process.env.REACT_APP_ASTRO_APP_SECRET;
 const hash = Buffer.from(`${appId}:${appSecret}`).toString('base64')
 const header = {
         //   "X-Requested-With": "XMLHttpRequest",
