a8139523eae9a7072ba631a5a470eb7464482c10
wkropat <wkropat@gmail.com>
2021-12-06 01:12:02 -0800

    added bio

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Project from './project';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 // Build request URL
 const projects = [
@@ -28,9 +28,13 @@ function Main() {
 
   return (
     <Container className="text-center" fluid>
+      <Row><Card>A full stack developer in training.</Card></Row>
+      <br/>
       <Row>
         {projects.map((project) => (
+          <Col>
           <Project name={project.name} desc={project.description} url={project.url} img={project.img}></Project>
+          </Col>
         ))}
       </Row>
     </Container>
