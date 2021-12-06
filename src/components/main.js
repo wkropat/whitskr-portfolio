405c222f0643580cc8ad7d9c1a566bd67cc16abe
wkropat <wkropat@gmail.com>
2021-12-06 01:03:46 -0800

    links workign

--- src/components/main.js
+++ src/components/main.js
@@ -1,41 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Project from './project';
-import {Container, Row, Col }from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // Build request URL
 const projects = [
   {
     name: 'RecipeBuilder',
     description: 'Get recipes based on ingredients in your kitchen',
-    url: "github.com/wkropat",
+    url: "https://github.com/RecipeBuilder",
     img: "../assets/recipeBuilder.jpg"
   },
   {
     name: 'Andromeda',
     description: 'Space Weather Alert App',
-    url: "github.com/wkropat",
+    url: "https://github.com/lets-play-chess",
     img: "../assets/andromeda.jpg"
   },
   {
     name: 'Chess',
     description: 'Live Chess App',
-    url: "github.com/wkropat",
+    url: "https://github.com/Andromeda-App",
     img: "../assets/playChess.jpg"
   },
 ];
 function Main() {
-  
 
-    return (
+
+  return (
     <Container className="text-center" fluid>
       <Row>
-{projects.map((project) => (
-        <Project name={project.name} desc={project.description} url={project.url} img={project.img}></Project>
-      ))}
+        {projects.map((project) => (
+          <Project name={project.name} desc={project.description} url={project.url} img={project.img}></Project>
+        ))}
       </Row>
     </Container>
-    );
-  }
+  );
+}
 
 
 export default Main;
